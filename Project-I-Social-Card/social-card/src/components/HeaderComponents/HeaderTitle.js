ae37f5f27f4c7c4167ad924c628036f84cd313bb
Ronny Alvarado <rsalvarado777@gmail.com>
2019-02-27 19:23:18 -0600

    created the component for HeaderTitle, but no timestamp yet until further down the line

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderTitle () {
+    return (
+        <>
+            <h1>Lambda School</h1>
+            <h4>LambdaSchool</h4>
+        </>
+    );
+}
+
+export default HeaderTitle;
