169ff97ffc2ce207d405c7d379dcba1a8215d2e1
Ronny Alvarado <rsalvarado777@gmail.com>
2019-03-01 18:42:05 -0600

    created component for the calculator display

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Display.css';
+
+function CalculatorDisplay (props) {
+    return (
+        <React.Fragment>
+            <h1 className='calculatorDisplay'>0</h1>
+        </React.Fragment>
+    );
+};
+
+export default CalculatorDisplay;
